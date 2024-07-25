e2f571a8e95e68c20836063571dfe766328546d4
umair0033 <umairgcs033@gmail.com>
2024-07-25 16:52:41 +0500

    change words

--- src/Components/Policy/Policy.jsx
+++ src/Components/Policy/Policy.jsx
@@ -6,13 +6,13 @@ const Policy = () => {
     <>
     <div className='policy'>
       <h1>Privacy Policy</h1>
-      <p>Cheezious Menu Price operates the https://www.cheeziousmenuprice.com website, which provides the SERVICE.
+      <p>cheezywrap Menu Price operates the https://www.cheezywrap.com website, which provides the SERVICE.
 
-This page is used to inform website visitors regarding our policies with the collection, use, and disclosure of Personal Information if anyone decided to use our Service, the Cheezious Menu Price website.
+This page is used to inform website visitors regarding our policies with the collection, use, and disclosure of Personal Information if anyone decided to use our Service, the cheezywrap Menu Price website.
 
 If you choose to use our Service, then you agree to the collection and use of information in relation with this policy. The Personal Information that we collect are used for providing and improving the Service. We will not use or share your information with anyone except as described in this Privacy Policy.
 
-The terms used in this Privacy Policy have the same meanings as in our Terms and Conditions, which is accessible at https://www.cheeziousmenuprice.com, unless otherwise defined in this Privacy Policy.</p>
+The terms used in this Privacy Policy have the same meanings as in our Terms and Conditions, which is accessible at https://www.cheezywrap.com, unless otherwise defined in this Privacy Policy.</p>
     </div>
 
     <div className='head-2'>
